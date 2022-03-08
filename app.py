0d12743cd31cc575851ff2a65aa7d8e3b44238bf
BarbaraLee <937689229@qq.com>
2022-03-08 19:48:55 +0800

    Add 4 more model

--- app.py
+++ app.py
@@ -17,22 +17,24 @@ def index():
         print(Nikkei)
         model1 = joblib.load("STI_REG")
         pred1 = model1.predict([[Nikkei]])
-        str1 = "The prediction for STI using Regression is : " + str(pred1)
-        model2 = joblib.load("STI_REG")
+        str1 = "The prediction for STI using Linear Regression is : " + \
+            str(pred1)
+        model2 = joblib.load("DT_REG")
         pred2 = model2.predict([[Nikkei]])
-        str2 = "The prediction for STI using Regression is : " + str(pred2)
-        model3 = joblib.load("STI_REG")
+        str2 = "The prediction for STI using Decision Tree is : " + str(pred2)
+        model3 = joblib.load("SVM_REG")
         pred3 = model3.predict([[Nikkei]])
-        str3 = "The prediction for STI using Regression is : " + str(pred3)
-        model4 = joblib.load("STI_REG")
+        str3 = "The prediction for STI using Support Vector Machine is : " + \
+            str(pred3)
+        model4 = joblib.load("RF_REG")
         pred4 = model4.predict([[Nikkei]])
-        str4 = "The prediction for STI using Regression is : " + str(pred4)
-        model5 = joblib.load("STI_REG")
+        str4 = "The prediction for STI using Random Forest is : " + str(pred4)
+        model5 = joblib.load("NN_REG")
         pred5 = model5.predict([[Nikkei]])
-        str5 = "The prediction for STI using Regression is : " + str(pred5)
+        str5 = "The prediction for STI using Nerual Network is : " + str(pred5)
         return(render_template("index.html", result1=str1, result2=str2, result3=str3, result4=str4, result5=str5))
     else:
-        return(render_template("index.html", result1="2"))
+        return(render_template("index.html", result1="Hello"))
 
 
 # %%
